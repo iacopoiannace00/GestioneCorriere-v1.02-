61348503c6c56e81f424bbf68bca5bf179f9563e
iacopoiannace00 <44878597+iacopoiannace00@users.noreply.github.com>
2018-11-08 20:18:25 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Name: Iacopo Iannace
+ * Class: 4Ai
+ * Date: 08/11/2018
+ * Project: Gestione Corriere v1.02 (Java)
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int a;
+	}
+
+}
